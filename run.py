44b1570228e0eeb5f9ef849d6e79bcff5b3b3af0
josecramirez <josecramirez.ml@gmail.com>
2019-01-25 16:54:30 -0600

    Generate a random string provided a selection of characters.
    The program will simply generate a random string of characters
    using all letters (uppercase and lowercase), digits, and
    punctuations.

--- run.py
+++ run.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+
+import random
+import string
+
+def generate_pwd():
+    pwd = ''
+    pwd_len = random.randrange(8, 16)
+    selection = string.ascii_letters + string.digits + string.punctuation
+
+    for i in range(pwd_len):
+        pwd += random.choice(selection)
+
+    return pwd
+
+if __name__ == "__main__":
+    print(f"Your randomly generated password is: { generate_pwd() }")
